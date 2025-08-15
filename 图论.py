522530e8f0c11a49621d8f0981692aad2bafcfba
yicaihui <134593175+yicaihui@users.noreply.github.com>
2025-08-15 22:15:12 +0800

    Update 图论.py

--- 图论.py
+++ 图论.py
@@ -1,39 +1,86 @@
-# prim 算法
-# 无向有权图，解决最小生成树问题
-# 找到距离最小生成树的最短距离，更新距离
-
-# dijkastra 算法
-# 有向有权图，解决最短路径问题
-# 找到距离源点的最短距离，更新距离
+# dijkstra 算法 两个for循环
 def main():
     N,M = map(int,input().split())
-    grid = [[float('inf')] * (N + 1) for _ in range(N + 1)]
+    records = []
     for _ in range(M):
-        S,E,V = map(int,input().split())
-        grid[S][E] = V
+        records.append(map(int,input().split()))
     
+    # 邻接矩阵或邻接表
+    grid = [[float('inf')] * (N + 1) for _ in range(N + 1)]
+    for S,E,V in records:
+        grid[S][E] = V
+
+    # 记录某个节点距离源点的最短距离
+    mindists = [float('inf')] * (N + 1)
+    mindists[1] = 0
+    # 记录是否访问过
     visited = [False] * (N + 1)
-    mindist = [float('inf')] * (N + 1)
-    mindist[1] = 0
-    for _ in range(1,N + 1):
+
+    # parents记录路径
+    parents = [-1] * (N + 1)
+
+    for _ in range(N):
         cur = -1
-        minval = float('inf')
+        mindist = float('inf')
         for i in range(1,N + 1):
-            if not visited[i] and mindist[i] < minval:
+            if not visited[i] and mindists[i] < mindist:
                 cur = i
-                minval = mindist[i]
-        # 如果没有找到合适的一点，提前结束循环
+                mindist = mindists[i]
+        
         if cur == -1:
             break
+        
         visited[cur] = True
-        # 更新距离
+
         for j in range(1, N + 1):
-            if not visited[j] and mindist[j] > mindist[cur] + grid[cur][j]:
-                mindist[j] = mindist[cur] + grid[cur][j]
-    if mindist[N] == float('inf'):
-        print('-1')
+            if not visited[j] and mindists[j] > mindists[cur] + grid[cur][j]:
+                mindists[j] = mindists[cur] + grid[cur][j]
+                parents[j] = cur
+
+    if mindists[N] == float('inf'):
+        print(-1)
     else:
-        print(mindist[N])
+        print(mindists[N])
 
+# dijkstra 堆优化版
+from collections import defaultdict
+import heapq
 
-main()
+def main():
+    N,M = map(int,input().split())
+    records = []
+    for _ in range(M):
+        records.append(map(int,input().split()))
+    
+    # 邻接矩阵或邻接表
+    mapper = defaultdict(list)
+    for s,e,val in records:
+        mapper[s].append([e,val])
+
+    # 记录某个节点距离源点的最短距离
+    mindists = [float('inf')] * (N + 1)
+    mindists[1] = 0
+    # 记录是否访问过
+    visited = [False] * (N + 1)
+    # 初始化优先队列
+    # 添加元组(priority, value)
+    pq = []
+    heapq.heappush(pq,(0,1))
+
+    while pq:
+        dist,cur = heapq.heappop(pq)
+        
+        if visited[cur]:
+            continue
+
+        visited[cur] = True
+
+        for e,val in mapper[cur]:
+            if not visited[e] and mindists[e] > dist + val:
+                mindists[e] = dist + val
+                heapq.heappush(pq,(mindists[e],e))
+
+    if mindists[N] == float('inf'):
+        print(-1)
+    else:
+        print(mindists[N])
