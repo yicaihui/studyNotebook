3f26567a7441a1cd0d7f13a97ea50fd4617f6712
yicaihui <134593175+yicaihui@users.noreply.github.com>
2025-08-19 21:57:10 +0800

    Update 图论.py

--- 图论.py
+++ 图论.py
@@ -84,3 +84,63 @@ def main():
         print(-1)
     else:
         print(mindists[N])
+
+# bellman ford算法
+def main():
+    n,m = map(int,input().split())
+    edges = []
+    for _ in range(m):
+        s,e,val = map(int,input().split())
+        edges.append([s,e,val])
+
+    mindist = [float('inf')] * (n + 1)
+    mindist[1] = 0
+    
+    for _ in range(n - 1):
+        update = False
+        for s,e,val in edges:
+            if mindist[s] != float('inf') and mindist[e] > mindist[s] + val:
+                update = True
+                mindist[e] = mindist[s] + val
+
+        if not update:
+            break
+    
+    if mindist[n] == float('inf'):
+        print('unconnected')
+    else:
+        print(mindist[n])
+
+# SPFA 算法
+from collections import defaultdict
+
+def main():
+    n,m = map(int,input().split())
+    mapper = defaultdict(list)
+    for _ in range(m):
+        s,e,val = map(int,input().split())
+        mapper[s].append([e,val])
+
+    # 记录最小距离
+    mindist = [float('inf')] * (n + 1)
+    # 记录是否已经在队列里，不需要重复添加
+    visited = [False] * (n + 1)
+    queue = []
+    mindist[1] = 0
+    queue.append(1)
+    visited[1] = True
+    
+    while queue:
+        cur = queue.pop(0)
+        visited[cur] = False
+        for e,val in mapper[cur]:
+            if mindist[e] > mindist[cur] + val:
+                mindist[e] = mindist[cur] + val
+                if not visited[e]:
+                    visited[e] = True
+                    queue.append(e)
+    
+    if mindist[n] == float('inf'):
+        print('unconnected')
+    else:
+        print(mindist[n])
